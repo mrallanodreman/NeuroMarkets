2cd4fb73d0179a04373d69d0223c37bda2ce818f
Mr.Polemics <43389674+mrallanodreman@users.noreply.github.com>
2025-01-19 23:03:16 -0500

    Archivos Principales
    Deben ir juntos en una misa ubicación, ya que estos  interactuan  entre ellos.

---
+++ EthConfig.py
@@ -0,0 +1,9 @@
+# Variables de configuración
+BASE_URL = "https://demo-api-capital.backend-capital.com/"
+SESSION_ENDPOINT = "/api/v1/session"
+MARKET_SEARCH_ENDPOINT = "/api/v1/markets"
+API_KEY = "dshUTxIDbHEtaOJS"
+LOGIN = "ODREMANALLANR@GMAIL.COM"
+PASSWORD = "Millo2025."
+DATA_FILE = "/home/hobeat/MoneyMakers/BTC-USD_IndicadoresCondensados/"
+DATA_DIR = "Reports"  # Directorio donde se guardarán los archivos JSON
