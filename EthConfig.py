59b26fc8e0f120a6a78da15bfcbfaaf53e827469
Mr.Polemics <43389674+mrallanodreman@users.noreply.github.com>
2025-01-19 23:05:03 -0500

    Update EthConfig.py
    Tus Credenciales de acceso deben ir configuradas aqui

--- EthConfig.py
+++ EthConfig.py
@@ -2,8 +2,7 @@
 BASE_URL = "https://demo-api-capital.backend-capital.com/"
 SESSION_ENDPOINT = "/api/v1/session"
 MARKET_SEARCH_ENDPOINT = "/api/v1/markets"
-API_KEY = "dshUTxIDbHEtaOJS"
-LOGIN = "ODREMANALLANR@GMAIL.COM"
-PASSWORD = "Millo2025."
-DATA_FILE = "/home/hobeat/MoneyMakers/BTC-USD_IndicadoresCondensados/"
+API_KEY = "LLAVE API DE CAPITAL.COM "
+LOGIN = "CORREO LOGIN "
+PASSWORD = "CLAVE DE USO DE API (LA QUE USAS AL CREAR LA API) "
 DATA_DIR = "Reports"  # Directorio donde se guardarán los archivos JSON
