8e380b06d17ded1539b3248038d8e57c2fa2f77f
Mr.Polemics <43389674+mrallanodreman@users.noreply.github.com>
2025-01-21 21:33:25 -0500

    Update EthConfig.py

--- SharkBoy/EthConfig.py
+++ SharkBoy/EthConfig.py
@@ -1,9 +1,9 @@
 # Variables de configuración
-BASE_URL = " https://demo-api-capital.backend-capital.com/"
+BASE_URL = "https://demo-api-capital.backend-capital.com/"
 SESSION_ENDPOINT = "/api/v1/session"
 MARKET_SEARCH_ENDPOINT = "/api/v1/markets"
-API_KEY = "dshUTxIDbHEtaOJS"
-LOGIN = "ODREMANALLANR@GMAIL.COM"
-PASSWORD = "Millo2025."
+API_KEY = "TU API KEY "
+LOGIN = "TU CORREO "
+PASSWORD = "TU CLAVE API "
 DATA_FILE = "/home/hobeat/MoneyMakers/BTC-USD_IndicadoresCondensados/"
 DATA_DIR = "Reports"  # Directorio donde se guardarán los archivos JSON
