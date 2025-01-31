dd1abdaa3381e7981e5ae324bdba2e5718d4cec1
Mr.Polemics <43389674+mrallanodreman@users.noreply.github.com>
2025-01-30 20:46:30 -0500

    Actualizacion de SharkBoy

--- SharkBoy/EthConfig.py
+++
@@ -1,9 +0,0 @@
-# Variables de configuración
-BASE_URL = "https://demo-api-capital.backend-capital.com/"
-SESSION_ENDPOINT = "/api/v1/session"
-MARKET_SEARCH_ENDPOINT = "/api/v1/markets"
-API_KEY = "TU API KEY "
-LOGIN = "TU CORREO "
-PASSWORD = "TU CLAVE API "
-DATA_FILE = "/home/hobeat/MoneyMakers/BTC-USD_IndicadoresCondensados/"
-DATA_DIR = "Reports"  # Directorio donde se guardarán los archivos JSON
