39e6a2ee92b23b923e26984315236b1a98a14ca7
Mr.Polemics <43389674+mrallanodreman@users.noreply.github.com>
2025-02-08 16:49:59 -0500

    carmelinupload

---
+++ RatzoBot/CarmelinOPerator/EthConfig.py
@@ -0,0 +1,18 @@
+# Variables de configuración
+BASE_URL = "https://demo-api-capital.backend-capital.com/"
+SESSION_ENDPOINT = "/api/v1/session"
+MARKET_SEARCH_ENDPOINT = "/api/v1/markets"
+API_KEY = "flYX0WLjUrgzwpow"
+LOGIN = "rangelcarmelo89@gmail.com"
+PASSWORD = "Bots253890@"
+DATA_FILE = "/home/hobeat/MoneyMakers/BTC-USD_IndicadoresCondensados/"
+DATA_DIR = "Reports"  # Directorio donde se guardarán los archivos JSON
+
+
+
+#Carmelo:
+#Api Key : flYX0WLjUrgzwpow
+#[19/01, 23:49] Carmelo: BotsDemo
+#[19/01, 23:50] Carmelo: rangelcarmelo89@gmail.com
+#[19/01, 23:52] Carmelo: Capital253890@
+#[19/01, 23:56] Carmelo: password api: Bots253890@
